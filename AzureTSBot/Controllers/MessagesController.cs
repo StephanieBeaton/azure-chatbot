617a4f8567a66bab84e6b070a7c67416cc60fdf5
Stephanie Beaton <stefanybeaton@gmail.com>
2016-07-15 09:54:42 -0700

    form from json - version 2

---
+++ AzureTSBot/Controllers/MessagesController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AzureTSBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        internal static IDialog<JObject> MakeJsonRootDialog()
+        {
+            return Chain.From(() => FormDialog.FromForm(TestForm.BuildJsonForm))
+                .Do(async (context, answer) =>
+                {
+                    try
+                    {
+                        var completed = await answer;
+
+                        await context.PostAsync("Processed your answer!");
+                    }
+                    catch (FormCanceledException<JObject> e)
+                    {
+                        string reply;
+                        if (e.InnerException == null)
+                        {
+                            reply = $"You quit on {e.Last}--maybe you can finish next time!";
+                        }
+                        else
+                        {
+                            reply = "Sorry, I've had a short circuit. Please try again.";
+                        }
+                        await context.PostAsync(reply);
+                    }
+                });
+
+        }
+
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, MakeJsonRootDialog);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
