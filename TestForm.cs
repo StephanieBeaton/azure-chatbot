948646f1deee083e2c375ec746a89586a5248132
Stephanie Beaton <stefanybeaton@gmail.com>
2016-07-14 18:32:25 -0700

    Bot with FormFlow form created from JSON

---
+++ TestForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.FormFlow.Json;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Web.Http.Description;
+using System.Web.Http;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Reflection;
+using System.Web;
+
+namespace AzureTSBot
+{
+    public class TestForm
+    {
+        public static IForm<JObject> BuildJsonForm()
+        {
+            //var stream = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AzureTSBot.AzureTroubleshootingForm.json"))
+            {
+                var schema = JObject.Parse(new StreamReader(stream).ReadToEnd());
+                return new FormBuilderJson(schema)
+                    .AddRemainingFields()
+                    .Build();
+            }
+        }
+    }
+}
